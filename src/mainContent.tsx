d09300d49771f9623efd4f06a896b767fbd7c411
yasser lakrid <yasserlakrid36@gmail.com>
2026-02-21 17:47:41 +0100

    handle loading

--- src/mainContent.tsx
+++ src/mainContent.tsx
@@ -1,34 +1,55 @@
 import "./mainContent.css"
-import { useEffect, useState } from "react"
+import { use, useEffect, useState } from "react"
 const accessKey = "SsfwWHtRiHXuJAnN03Roxxd5Fyq-aA4I2DNm0nlEzVI"
 
 function MainContent() {
     const [bottom , reachedBottom ] = useState(false);
+    const [loading , setLoading] = useState(true)
 const [photos , setPhotos] = useState([])
     useEffect(()=>{
         const req = async ()=>{
             try{
                const reqs =  await fetch("https://api.unsplash.com/photos/random?count=15&query=coffee&orientation=landscape",{
                 headers: {
-              Authorization: `Client-ID ${accessKey}`
+                Authorization: `Client-ID ${accessKey}`
             }
             })
+              if (!reqs.ok) {
+    const text = await reqs.text();
+    console.log("Error response:", text);
+    return;
+  }
+
             const data = await reqs.json() 
             setPhotos(data)
             
-            console.log("the pic are available", data)
+            setLoading(false)
+        
+           
             }catch(err){
                 console.log("error fetching", err)
+                 
             }
            
 
         }
         req()
        
+
     },[])
     return (
         <div className="MainContentVid">
-            {photos.length === 0 ? (<div className="loader">Loading ... </div> ): (
+            {
+                loading ? (
+                    <div className="loadingAniamtion">
+                        loading
+                    </div>
+                ):(
+                    <>
+                    </>
+                )
+            }
+            {photos.length === 0 ? (<></> ): (
             photos.map((e : any ,index)=>(
                     <div key={index}>
                         <img src={e.urls.regular} >
