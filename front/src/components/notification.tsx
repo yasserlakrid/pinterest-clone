40cec2e63362bf27568db3aa912c0de9fb178dcd
yasser lakrid <yasserlakrid36@gmail.com>
2026-07-11 12:45:18 +0100

    build'

--- front/src/components/notification.tsx
+++ front/src/components/notification.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 function Notification () {
    const item = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
